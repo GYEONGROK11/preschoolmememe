e028b302975182e1a54202565f089c355bd49220
GYEONGROK11 <gpol123@naver.com>
2024-01-11 18:08:58 +0900

    me

---
+++ src/main/java/com/meme/preschoolmememe/PreschoolmememeApplication.java
@@ -0,0 +1,13 @@
+package com.meme.preschoolmememe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreschoolmememeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreschoolmememeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meme/preschoolmememe/PreschoolmememeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meme.preschoolmememe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreschoolmememeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
